ddc377bad76e04cddb0d9df9fb326788d387c655
nileninja <156793196+nileninja-1@users.noreply.github.com>
2025-12-25 19:59:59 +0300

    Return HTML response instead of plain text

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 module.exports = function (cb) {
-  cb(null, 'hello nileninja!');
-}
+  cb(null, {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8'
+    },
+    body: `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Auth0 Extension</title>
+        </head>
+        <body>
+          <h1>Hello nileninja!</h1>
+        </body>
+      </html>
+    `
+  });
+};
