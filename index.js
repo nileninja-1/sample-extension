4614ee4b86d36428f9027d45d2e206f9a130b415
nileninja <156793196+nileninja-1@users.noreply.github.com>
2025-12-25 23:12:07 +0300

    Change response to return 'Hello world'
    Refactored the module to return a simple string instead of an HTML response.

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 module.exports = function (cb) {
-  cb(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8'
-    },
-    body: `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>Auth0 Extension</title>
-        </head>
-        <body>
-          <h1>Hello nileninja!</h1>
-        </body>
-      </html>
-    `
-  });
+  return "Hello world";
 };
